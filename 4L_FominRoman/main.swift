12934abd18daf62834e5a68ec4883fce23dfa0cc
romanf <fomin.tp21@mail.ru>
2022-01-30 19:45:14 +0300

    описал классы грузового и спорт автомобилей

--- 4L_FominRoman/main.swift
+++ 4L_FominRoman/main.swift
@@ -1,7 +1,146 @@
-//1. Описать класс Car c общими свойствами автомобилей и пустым методом действия по аналогии с прошлым заданием.
-//
+
 
 import Foundation
 
+//1. Описать класс Car c общими свойствами автомобилей и пустым методом действия по аналогии с прошлым заданием.
+
+enum EngineStatus {  // состояние двигателя
+    case stop, start
+    }
+
+enum WindowSatus {   // состояние окон.
+    case open, close
+    }
+
+enum BagageStatus {  // погрузка или разгрузка авто
+    case load, unload
+    }
+
+enum TransmissionStatus {   // состояние трансмиссии
+    case parking, neutral, drive
+    }
+
+
+class TrunkCar {
+    
+    let brand: String   //  марка
+    let power: Int      // мощность
+    let year: Int       // год
+    let bagageMax: Int  // грузоподъемность авто
+    var bagageWeight: Int   // вес груза в машине
+    var cargo: Int          //вес груза, который хотим погрузить
+    var engineStart: EngineStatus
+    var windowOpen: WindowSatus
+    var bagage: BagageStatus
+    
+    init (brand: String, power: Int, year: Int,bagageMax: Int,bagageWeight: Int, cargo: Int, engineStart: EngineStatus, windowOpen: WindowSatus, bagage: BagageStatus) {
+     
+        self.brand = brand
+        self.power = power
+        self.year = year
+        self.bagageMax = bagageMax
+        self.bagageWeight = bagageWeight
+        self.cargo = cargo
+        self.engineStart = engineStart
+        self.windowOpen = windowOpen
+        self.bagage = bagage
+    }
+}
+
+class sportCar{
+        let brand: String
+        let year: Int
+        let weightCar: Int  // вес авто
+        let weightTotal: Int    // общий вес: авто + груз
+        var weightBagage: Int   // вес груза
+        let window: WindowSatus
+        var engine: EngineStatus
+        var transmission: TransmissionStatus
+
+    init( brand: String, year: Int, weightCar: Int, weightTotal: Int, weightBagage: Int, window: WindowSatus, engine: EngineStatus, transmission: TransmissionStatus ) {
+        
+        self.brand = brand
+        self.year = year
+        self.weightCar = weightCar
+        self.weightTotal = weightTotal
+        self.weightBagage = weightBagage
+        self.window = window
+        self.engine = engine
+        self.transmission = transmission
+    }
+        
+
+
+    
+//    // включить- выключить двигатель
+//    mutating func engine (engineStart: EngineStatus ) {
+//        self.engineStart = engineStart
+//    }
+//
+//    // загрузить или выгрузить  количество кг в авто
+//    mutating func operationBagage (operation: BagageStatus, cargoBagage: Int ) {
+//
+//        switch operation {
+//        case .load: do {
+//            if (cargoBagage + self.bagageWeight) > self.bagageMax {
+//                print("Операция отменена. Нельзя \(cargoBagage) кг загрузить в авто, будет перегруз.")
+//            } else {
+//                self.bagageWeight = self.bagageWeight + cargoBagage
+//                self.cargo = cargoBagage
+//                print("В автомобиль загрузили \(cargoBagage) кг")
+//            }
+//        }
+//        case .unload: do {
+//            if (self.bagageWeight) < cargoBagage {
+//                print("нельзя \(cargoBagage) кг выгрузить из авто")
+//            } else {
+//                self.bagageWeight = self.bagageWeight - cargoBagage
+//                self.cargo = cargoBagage
+//                print("Из автомобиля выгрузили \(cargoBagage) кг")
+//            }
+//        }
+//        }
+//    }
+//}
+//
+//struct  sportCar{
+//        let brand: String
+//        let year: Int
+//        let weightCar: Int  // вес авто
+//        let weightTotal: Int    // общий вес: авто + груз
+//        var weightBagage: Int   // вес груза
+//        let window: WindowSatus
+//        var engine: EngineStatus
+//        var transmission: TransmissionStatus
+//
+//    mutating func autoStart (transmission: TransmissionStatus){
+//
+//        if transmission == .drive {
+//            print ("Автозапуск двигателя невозможен, трансмиссия находится в режиме драйв. Установите в положение 'нейтраль' или Parking")
+//
+//        } else {
+//            self.transmission = transmission
+//            self.engine = .start
+//            print("Автозапуск двигателя произведен")
+//        }
+//
+//    }
+//
+//}
+//
+////3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+//
+//
+//// операции с грузовым автомобилем
+//
+//var car1 = TrunkCar(brand: "Kamaz", power: 300, year: 2019, bagageMax: 25_000, bagageWeight: 15_000, cargo: 5_000, engineStart: .stop, windowOpen: .close, bagage: .unload)
+//
+//print(car1)
+//car1.bagage = .unload
+//car1.windowOpen = .open
+//car1.engine(engineStart: .start)
+//car1.operationBagage(operation: .unload, cargoBagage: 12_000)
+//car1.operationBagage(operation: .load, cargoBagage: 25_000)
+//print(car1)
 
 
